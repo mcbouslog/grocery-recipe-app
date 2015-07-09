18ca6877d63137f63f40834b6fe34763ce6ae3d8
Mark Bouslog <mcbouslog@gmail.com>
2015-07-08 21:57:21 -0500

    Sending multiple checkboxes in params

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -3,5 +3,23 @@ class GroceriesController < ApplicationController
   def index
     @produces = Grocery.where(grocery_category: "produce")
   end
+
+  def create
+  end
+
+  def show
+    @produces = Grocery.where(grocery_category: "produce")
+  end
+
+  def edit    
+  end
+
+  def update
+    user_groceries = User_grocery.where(user_id: current_user.id)
+    # user_groceries.each do |user_grocery|
+    #   user_grocery.destroy
+    # end
+
+  end
   
 end
