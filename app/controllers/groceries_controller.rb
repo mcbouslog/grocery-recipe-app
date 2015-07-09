b5eb1bcccde1445794f41b3973bba09e691dac6f
Mark Bouslog <mcbouslog@gmail.com>
2015-07-08 23:44:15 -0500

    Saved groceries to user_groceries db

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -2,6 +2,7 @@ class GroceriesController < ApplicationController
 
   def index
     @produces = Grocery.where(grocery_category: "produce")
+    @meats = Grocery.where(grocery_category: "meats")
   end
 
   def create
@@ -15,11 +16,15 @@ class GroceriesController < ApplicationController
   end
 
   def update
-    user_groceries = User_grocery.where(user_id: current_user.id)
-    # user_groceries.each do |user_grocery|
-    #   user_grocery.destroy
-    # end
-
+    user_groceries = UserGrocery.where(user_id: current_user.id)
+    user_groceries.each do |user_grocery|
+      user_grocery.destroy
+    end
+    user_groceries = params[:user_groceries]
+    user_groceries.each do |user_grocery|
+      UserGrocery.create(user_id: current_user.id, grocery_id: user_grocery, stock: true)
+    end
+    redirect_to "/groceries/#{current_user.id}"
   end
   
 end
