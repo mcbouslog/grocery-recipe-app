94ea6d6472a6b8034358abbe386521464550cc28
Mark Bouslog <mcbouslog@gmail.com>
2015-07-30 11:28:27 -0500

    misc recipe search updates

--- app/assets/javascripts/recipes_Ctrl.js
+++ app/assets/javascripts/recipes_Ctrl.js
@@ -50,8 +50,8 @@
       $http.post('/api/v1/api_searches.json', searchStringHash).then(function(response) {
           
         $scope.searchResults = response.data;
-        $scope.attribution = $scope.searchResults["attribution"]["html"];
-        $scope.matches = $scope.searchResults["matches"];
+        $scope.attribution = $scope.searchResults.attribution.html;
+        $scope.matches = $scope.searchResults.matches;
         
 // FILTERS START ******
 
@@ -59,18 +59,18 @@
           $scope.cuisines = [];
           $scope.courses = [];          
           for (var l = 0; l < matches.length; l++) {
-            if (matches[l]["attributes"] !== undefined) {
-              if (matches[l]["attributes"]["cuisine"] !== undefined) {
-                for (var m = 0; m < matches[l]["attributes"]["cuisine"].length; m++) {
-                  if ($scope.cuisines.indexOf(matches[l]["attributes"]["cuisine"][m]) == -1) {
-                    $scope.cuisines.push(matches[l]["attributes"]["cuisine"][m]);
+            if (matches[l].attributes !== undefined) {
+              if (matches[l].attributes.cuisine !== undefined) {
+                for (var m = 0; m < matches[l].attributes.cuisine.length; m++) {
+                  if ($scope.cuisines.indexOf(matches[l].attributes.cuisine[m]) == -1) {
+                    $scope.cuisines.push(matches[l].attributes.cuisine[m]);
                   };
                 };
               };
-              if (matches[l]["attributes"]["course"] !== undefined) {
-                for (var n = 0; n < matches[l]["attributes"]["course"].length; n++) {
-                  if ($scope.courses.indexOf(matches[l]["attributes"]["course"][n]) == -1) {
-                    $scope.courses.push(matches[l]["attributes"]["course"][n]);
+              if (matches[l].attributes.course !== undefined) {
+                for (var n = 0; n < matches[l].attributes.course.length; n++) {
+                  if ($scope.courses.indexOf(matches[l].attributes.course[n]) == -1) {
+                    $scope.courses.push(matches[l].attributes.course[n]);
                   };
                 };
               };
@@ -86,38 +86,37 @@
         $scope.filterTime = function(entry) {
           if($scope.filterTimeOption.filterTimeValue) {
             return (entry.totalTimeInSeconds <= parseInt($scope.filterTimeOption.filterTimeValue)) ? true: false;
-          };
-          
+          };          
           return true;
         };
+        
         $scope.filterScoreOption = {
           filterScoreValue: false,
         };
         $scope.filterScore = function(entry) {
           if($scope.filterScoreOption.filterScoreValue) {
             return (entry.matchScore >= parseInt($scope.filterScoreOption.filterScoreValue)) ? true: false;
-          };
-          
+          };          
           return true;
         };
+        
         $scope.filterUnIngOption = {
           filterUnIngValue: false,
         };
         $scope.filterUnIng = function(entry) {
           if($scope.filterUnIngOption.filterUnIngValue) {
             return (entry.unMatchIngCount <= parseInt($scope.filterUnIngOption.filterUnIngValue)) ? true: false;
-          };
-          
+          };          
           return true;
         };
+        
         $scope.filterRatingOption = {
           filterRatingValue: false,
         };
         $scope.filterRating = function(entry) {
           if($scope.filterRatingOption.filterRatingValue) {
             return (entry.rating >= parseInt($scope.filterRatingOption.filterRatingValue)) ? true: false;
-          };
-          
+          };          
           return true;
         };
 
@@ -127,30 +126,30 @@
         
         $scope.matchCount = function(match) {
           $scope.matchIngCount = 0;
-          for (var k = 0; k < match["ingredients"].length; k++) {
-            if ($scope.ingredients[match["ingredients"][k]]) {
-              if ($scope.ingredients[match["ingredients"][k]]["currentUser"]) {
+          for (var k = 0; k < match.ingredients.length; k++) {
+            if ($scope.ingredients[match.ingredients[k]]) {
+              if ($scope.ingredients[match.ingredients[k]].currentUser) {
                 $scope.matchIngCount++;
               };
             };
           };
-          $scope.unMatchIngCount = match["ingredients"].length - $scope.matchIngCount;
+          $scope.unMatchIngCount = match.ingredients.length - $scope.matchIngCount;
           return $scope.unMatchIngCount;
         };
 
         $scope.matchTime = function(match) {
-          $scope.matchMinutes = match["totalTimeInSeconds"] / 60;
+          $scope.matchMinutes = match.totalTimeInSeconds / 60;
           return $scope.matchMinutes;
         };
 
         $scope.matchScoreCalc = function(match) {
           $scope.scoreArray = [];
-          for (var o = 0; o < match["ingredients"].length; o++) {
-            if ($scope.ingredients[match["ingredients"][o]]) {
-              if ($scope.ingredients[match["ingredients"][o]]["currentUser"] === true) {
+          for (var o = 0; o < match.ingredients.length; o++) {
+            if ($scope.ingredients[match.ingredients[o]]) {
+              if ($scope.ingredients[match.ingredients[o]].currentUser === true) {
                 $scope.scoreArray.push(1);  
               } else {
-                $scope.scoreArray.push(parseFloat($scope.ingredients[match["ingredients"][o]]["scoreFactor"]));
+                $scope.scoreArray.push(parseFloat($scope.ingredients[match.ingredients[o]].scoreFactor));
               }
             } else {
               $scope.scoreArray.push(0.75);
